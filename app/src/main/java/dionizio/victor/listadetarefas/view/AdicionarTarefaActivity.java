7c387691c41da03f87d97ce0a29f5c7cc2e57141
Victor Dionizio <victor.dionizio28@gmail.com>
2021-12-21 11:25:47 -0300

    Primeiro commit

---
+++ app/src/main/java/dionizio/victor/listadetarefas/view/AdicionarTarefaActivity.java
@@ -0,0 +1,16 @@
+package dionizio.victor.listadetarefas.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import dionizio.victor.listadetarefas.R;
+
+public class AdicionarTarefaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicionar_tarefa);
+    }
+}
