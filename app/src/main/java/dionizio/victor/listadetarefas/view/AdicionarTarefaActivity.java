864f4d811cd00b052ab7d05512b139bb9e3eaecb
Victor Dionizio <victor.dionizio28@gmail.com>
2021-12-21 11:51:29 -0300

    Adicionando menu

--- app/src/main/java/dionizio/victor/listadetarefas/view/AdicionarTarefaActivity.java
+++ app/src/main/java/dionizio/victor/listadetarefas/view/AdicionarTarefaActivity.java
@@ -3,6 +3,7 @@ package dionizio.victor.listadetarefas.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 import dionizio.victor.listadetarefas.R;
 
@@ -12,5 +13,11 @@ public class AdicionarTarefaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_tarefa);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
